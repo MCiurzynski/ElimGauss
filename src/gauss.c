26f7353bf357466b01d77ba8e8af65ceea21a656
Mateusz <goldenfox1024@gmail.com>
2023-12-22 11:56:24 +0100

    Dodanio wybor elementu glownego

--- src/gauss.c
+++ src/gauss.c
@@ -1,17 +1,31 @@
 #include "gauss.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *a, Matrix *b){
-	int i, w, k;
-	double x;
+	int i, w, k, j;
+	double x, tmp;
 	if (a->r != a->c || b->r != a->r || b->c != 1)
 		return 1;
 	for (k = 0; k < a->r - 1; k++) {
-		for (w = k+1; w < a->r; w++){
+		for (w = k+1; w < a->r; w++) {
+			for (i = w; i < a->r; i++) {
+				if (abs(a->data[w][k]) < abs(a->data[i][k])) {
+					for (j = 0; j < a->r; j++) {
+						tmp = a->data[k][j];
+						a->data[k][j] = a->data[i][j];
+						a->data[i][j] = tmp;
+					}
+					tmp = b->data[k][0];
+					b->data[k][0] = b->data[i][0];
+					b->data[i][0] = tmp;
+				} 
+			}
 			x = a->data[w][k] / a->data[k][k];
 			for (i = k; i < a->r; i++) {
 				a->data[w][i] -= a->data[k][i]*x;
